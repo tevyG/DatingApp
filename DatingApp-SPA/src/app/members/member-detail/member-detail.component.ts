056278bbb58d3c2663a0c213b40f0d113ea6a638
tevyG <43766983+tevyG@users.noreply.github.com>
2018-10-09 11:15:38 -0400

    End of Section 10

--- DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-detail/member-detail.component.ts
@@ -18,7 +18,6 @@ export class MemberDetailComponent implements OnInit {
   constructor(private userService: UserService, private alertify: AlertifyService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    //this.loadUser();
     this.route.data.subscribe(data => {
       this.user = data['user'];
     });
@@ -50,12 +49,4 @@ export class MemberDetailComponent implements OnInit {
     }
     return imageUrls;
   }
-
-  // loadUser() {
-  //   this.userService.getUser(+this.route.snapshot.params['id']).subscribe((user: User) => {
-  //     this.user = user;
-  //   }, error => {
-  //     this.alertify.error(error);
-  //   });
-  // }
 }
--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -7,11 +7,12 @@ import { AuthGuard } from './_guards/auth.guard';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRoutes: Routes = [
     { path: '', component: HomeComponent},
-
-    //Protect multi Routings in once
     {
         path: '',
         runGuardsAndResolvers: 'always',
@@ -19,15 +20,11 @@ export const appRoutes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}},
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+            { path: 'member/edit', component: MemberEditComponent, resolve: {user: MemberEditResolver},
+                canDeactivate: [PreventUnsavedChanges]},
             { path: 'messages', component: MessagesComponent},
             { path: 'lists', component: ListsComponent},
         ]
     },
-
-    //Protect single Routing
-    //{ path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    //{ path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
-    //{ path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
-
     { path: '**', redirectTo: '', pathMatch: 'full'},
 ];
